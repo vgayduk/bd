8b308cd0e8e92d56b4976c0809f92f6442b78ff2
Vladyslav <vladyslav.gayduk@technorely.com>
2021-03-11 07:59:08 +0200

    Fix: Add scroll buttons

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,49 @@
+const sections = document.querySelectorAll(".parallaxParent");
+let currentSection = 0;
+
+const btnUp = document.querySelector("#button-up");
+const btnDown = document.querySelector("#button-down");
+
+const scrollEventHandler = (value) => {
+  if (value > 0 && currentSection < sections.length - 1) {
+    currentSection++;
+  } else if (value < 0 && currentSection > 0) {
+    currentSection--;
+  }
+  sections[currentSection].scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
+};
+
+sections[currentSection].scrollIntoView({
+  behavior: "smooth",
+  block: "start",
+});
+
+document.addEventListener(
+  "touchmove",
+  (e) => {
+    e.preventDefault();
+  },
+  { passive: false }
+);
+
+document.addEventListener(
+  "wheel",
+  (e) => {
+    e.preventDefault();
+  },
+  { passive: false }
+);
+
+btnUp.addEventListener("click", () => {
+  scrollEventHandler(-1);
+});
+btnDown.addEventListener("click", () => {
+  scrollEventHandler(1);
+});
+
 var controller = new ScrollMagic.Controller({
   globalSceneOptions: { triggerHook: "onEnter", duration: "200%" },
 });
@@ -14,3 +60,16 @@ new ScrollMagic.Scene({ triggerElement: "#parallax2" })
 new ScrollMagic.Scene({ triggerElement: "#parallax3" })
   .setTween("#parallax3 > div", { y: "80%", ease: Linear.easeNone })
   .addTo(controller);
+
+const triggers = document.querySelectorAll(".section-trigger");
+
+triggers.forEach((item) => {
+  new ScrollMagic.Scene({
+    triggerElement: `#${item.getAttribute("id")}`,
+    triggerHook: 0.9, // show, when scrolled 10% into view
+    duration: 0, // hide 10% before exiting view (80% + 10% from bottom)
+    offset: 10, // move trigger to center of element
+  })
+    .setClassToggle(`#${item.getAttribute("id")} + div`, "visible") // add class to reveal
+    .addTo(controller);
+});

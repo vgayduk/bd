ff0e289ffa85b3e5d85a20328a8eb676aad65d1c
Vladyslav <vladyslav.gayduk@technorely.com>
2021-03-14 00:31:34 +0200

    Feature: Add Sass & setup intro animations

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,17 +10,17 @@ sections[currentSection].scrollIntoView({
 });
 
 sections[currentSection].querySelector(".section-wrapper").classList.add("visible");
-let delay = 0.8;
+// let delay = 0.8;
 
-for (let i = 0; i < sections.length; i++) {
-  const messages = sections[i].querySelectorAll(".message-wrapper");
+// for (let i = 0; i < sections.length; i++) {
+//   const messages = sections[i].querySelectorAll(".message-wrapper");
 
-  for (let j = 0; j < messages.length; j++) {
-    messages[j].querySelector(".message").style.transitionDelay = `${delay}s`;
-    delay++;
-  }
-  delay = 0.8;
-}
+//   for (let j = 0; j < messages.length; j++) {
+//     messages[j].querySelector(".message").style.transitionDelay = `${delay}s`;
+//     delay++;
+//   }
+//   delay = 0.8;
+// }
 
 const scrollEventHandler = (value) => {
   if (value > 0 && currentSection < sections.length - 1) {
@@ -36,23 +36,29 @@ const scrollEventHandler = (value) => {
   });
 
   sections[currentSection].querySelector(".section-wrapper").classList.add("visible");
+
+  switch (sections[currentSection].getAttribute("id")) {
+    case "parallax1":
+      greetingAnimation();
+      break;
+    case "parallax2":
+      introAnimation();
+      break;
+    case "parallax3":
+      const promiseArr = [];
+      for (let i = 0; i < sections[currentSection].querySelectorAll(".message").length; i++) {
+        promiseArr[i] = messageTextAnimation.bind(
+          null,
+          `#${sections[currentSection].querySelectorAll(".message")[i].getAttribute("id")}`
+        );
+      }
+      // sections[currentSection].querySelectorAll(".message").map((item) => {
+      //   messageTextAnimation.bind(null, `#${item.getAttribute("id")}`);
+      // });
+      promiseArr.reduce((acc, fn) => acc.then(fn), Promise.resolve());
+      break;
+  }
 };
-
-document.addEventListener(
-  "touchmove",
-  (e) => {
-    e.preventDefault();
-  },
-  { passive: false }
-);
-
-document.addEventListener(
-  "wheel",
-  (e) => {
-    e.preventDefault();
-  },
-  { passive: false }
-);
 
 btnUp.addEventListener("click", () => {
   scrollEventHandler(-1);
@@ -78,33 +84,57 @@ new ScrollMagic.Scene({ triggerElement: "#parallax3" })
   .setTween("#parallax3 > div", { y: "80%", ease: Linear.easeNone })
   .addTo(controller);
 
-function pathPrepare($el) {
-  var lineLength = $el[0].getTotalLength();
-  $el.css("stroke-dasharray", lineLength);
-  $el.css("stroke-dashoffset", lineLength);
+$(document).ready(function () {
+  let messageid = 0;
+  $(".title").lettering();
+  $(".intro span").lettering();
+  sections.forEach((section) => {
+    section.querySelectorAll(".message").forEach((message) => {
+      message.setAttribute("id", `message_${messageid}`);
+      $(`#message_${messageid} span`).lettering();
+      messageid++;
+    });
+  });
+  greetingAnimation();
+});
+
+function greetingAnimation() {
+  var title1 = new TimelineMax();
+  title1.staggerFromTo(
+    ".title span",
+    0.5,
+    { ease: Back.easeOut.config(1.7), opacity: 0, bottom: -80 },
+    { ease: Back.easeOut.config(1.7), opacity: 1, bottom: 0 },
+    0.05
+  );
 }
 
-var $word = $("path#word");
-var $dot = $("path#dot");
+function introAnimation() {
+  var title2 = new TimelineMax();
+  title2.staggerFromTo(
+    ".intro span span",
+    0.5,
+    { ease: Back.easeOut.config(0.8), opacity: 0, left: -80 },
+    { ease: Back.easeOut.config(0.8), opacity: 1, left: 0 },
+    0.045
+  );
+}
 
-// prepare SVG
-pathPrepare($word);
-pathPrepare($dot);
-
-// init controller
-var controller = new ScrollMagic.Controller();
-
-// build tween
-var tween = new TimelineMax()
-  .add(TweenMax.to($word, 0.9, { strokeDashoffset: 0, ease: Linear.easeNone })) // draw word for 0.9
-  .add(TweenMax.to($dot, 0.1, { strokeDashoffset: 0, ease: Linear.easeNone })) // draw dot for 0.1
-  .add(TweenMax.to("path", 1, { stroke: "#33629c", ease: Linear.easeNone }), 0); // change color during the whole thing
-
-// build scene
-var scene = new ScrollMagic.Scene({
-  triggerElement: "#word",
-  duration: 200,
-  tweenChanges: true,
-})
-  .setTween(tween)
-  .addTo(controller);
+function messageTextAnimation(parent) {
+  return new Promise((resolve) => {
+    const timeForLetter = 0.04;
+    const message = new TimelineMax();
+    const totalTime = document.querySelectorAll(`${parent} span span`).length * timeForLetter;
+    document.querySelector(parent).classList.add("visible");
+    message.staggerFromTo(
+      `${parent} span span`,
+      0.1,
+      { opacity: 0 },
+      { opacity: 1 },
+      timeForLetter
+    );
+    setTimeout(() => {
+      resolve(totalTime);
+    }, totalTime * 750);
+  });
+}

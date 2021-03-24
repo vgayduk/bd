feb7182201121c64a9748898c8358c7f3fc0661c
Vladyslav Gayduk <vladyslav.gayduk@technorely.com>
2021-03-24 10:02:09 +0200

    Feature: Fill sections with content

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -41,11 +41,143 @@ const scrollEventHandler = (value) => {
       break;
     case "parallax3":
       descriptionAnimation(
-        `#${sections[currentSection].querySelector(".description").getAttribute("id")}`
+        `#${sections[currentSection].querySelector(".description").getAttribute("id")}`,
+        0.03
       );
       break;
     case "parallax4":
+      descriptionAnimation(
+        `#${sections[currentSection].querySelector(".description").getAttribute("id")}`
+      );
+      break;
+    case "parallax5":
       setVisibleSection();
+      break;
+    case "parallax6":
+      descriptionAnimation(
+        `#${sections[currentSection].querySelector(".description").getAttribute("id")}`,
+        0.03
+      );
+      break;
+    case "parallax7":
+      setVisibleSection();
+      break;
+    case "parallax8":
+      descriptionAnimation(
+        `#${sections[currentSection].querySelector(".description").getAttribute("id")}`,
+        0.03
+      );
+      break;
+    case "parallax9":
+      descriptionAnimation(
+        `#${sections[currentSection].querySelector(".description").getAttribute("id")}`,
+        0.03
+      );
+      break;
+    case "parallax10":
+      setVisibleSection();
+      break;
+    case "parallax11":
+      setVisibleSection();
+      break;
+    case "parallax12":
+      descriptionAnimation(
+        `#${sections[currentSection].querySelector(".description").getAttribute("id")}`,
+        0.03
+      );
+      break;
+    case "parallax13":
+      setVisibleSection();
+      break;
+    case "parallax14":
+      descriptionAnimation(
+        `#${sections[currentSection].querySelector(".description").getAttribute("id")}`,
+        0.03
+      );
+      break;
+    case "parallax15":
+      setVisibleSection();
+      break;
+    case "parallax16":
+      setVisibleSection();
+      break;
+    case "parallax17":
+      descriptionAnimation(
+        `#${sections[currentSection].querySelector(".description").getAttribute("id")}`,
+        0.03
+      );
+      break;
+    case "parallax18":
+      setVisibleSection();
+      break;
+    case "parallax19":
+      descriptionAnimation(
+        `#${sections[currentSection].querySelector(".description").getAttribute("id")}`,
+        0.03
+      );
+      break;
+    case "parallax20":
+      setVisibleSection();
+      break;
+    case "parallax21":
+      setVisibleSection();
+      break;
+    case "parallax22":
+      descriptionAnimation(
+        `#${sections[currentSection].querySelector(".description").getAttribute("id")}`,
+        0.03
+      );
+      break;
+    case "parallax23":
+      setVisibleSection();
+      break;
+    case "parallax24":
+      descriptionAnimation(
+        `#${sections[currentSection].querySelector(".description").getAttribute("id")}`,
+        0.03
+      );
+      break;
+    case "parallax25":
+      descriptionAnimation(
+        `#${sections[currentSection].querySelector(".description").getAttribute("id")}`,
+        0.03
+      );
+      break;
+    case "parallax26":
+      setVisibleSection();
+      break;
+    case "parallax27":
+      descriptionAnimation(
+        `#${sections[currentSection].querySelector(".description").getAttribute("id")}`,
+        0.03
+      );
+      break;
+    case "parallax28":
+      descriptionAnimation(
+        `#${sections[currentSection].querySelector(".description").getAttribute("id")}`,
+        0.03
+      );
+      break;
+    case "parallax29":
+      setVisibleSection();
+      break;
+    case "parallax30":
+      descriptionAnimation(
+        `#${sections[currentSection].querySelector(".description").getAttribute("id")}`,
+        0.03
+      );
+      break;
+    case "parallax31":
+      descriptionAnimation(
+        `#${sections[currentSection].querySelector(".description").getAttribute("id")}`,
+        0.03
+      );
+      break;
+    case "parallax32":
+      descriptionAnimation(
+        `#${sections[currentSection].querySelector(".description").getAttribute("id")}`,
+        0.03
+      );
       break;
   }
 };
@@ -85,6 +217,7 @@ new ScrollMagic.Scene({ triggerElement: "#parallax3" })
   .setTween("#parallax3 > div", { y: "80%", ease: Linear.easeNone })
   .addTo(controller);
 
+// Register messages below
 $(document).ready(function () {
   let messageid = 0;
   let descriptionid = 0;
@@ -128,28 +261,37 @@ function introAnimation() {
   );
 }
 
+// function messageTextAnimation(id) {
+//   return new Promise((resolve) => {
+//     const timeForLetter = 0.025;
+//     const message = new TimelineMax();
+//     const totalTime = document.querySelectorAll(`${id} span span`).length * timeForLetter;
+//     document.querySelector(id).classList.add("visible");
+//     message.staggerFromTo(`${id} span span`, 0.1, { opacity: 0 }, { opacity: 1 }, timeForLetter);
+//     if (!document.querySelector(id).parentNode.classList.contains("end"))
+//       setTimeout(() => {
+//         resolve(totalTime);
+//       }, totalTime * 950);
+//   });
+// }
+
 function messageTextAnimation(id) {
   return new Promise((resolve) => {
-    const timeForLetter = 0.025;
     const message = new TimelineMax();
-    const totalTime = document.querySelectorAll(`${id} span span`).length * timeForLetter;
     document.querySelector(id).classList.add("visible");
-    message.staggerFromTo(`${id} span span`, 0.1, { opacity: 0 }, { opacity: 1 }, timeForLetter);
-    console.log(document.querySelector(id).parentNode.classList.contains("end"));
-    if (!document.querySelector(id).parentNode.classList.contains("end"))
-      setTimeout(() => {
-        resolve(totalTime);
-      }, totalTime * 950);
+    message.staggerFromTo(`${id} span span`, 0.1, { opacity: 0 }, { opacity: 1 }, 0);
+    resolve();
   });
 }
 
-function descriptionAnimation(id) {
+function descriptionAnimation(id, letterSpeed = null) {
   const description = new TimelineMax();
   description.staggerFromTo(
     `${id} span span`,
     0.5,
     { ease: Back.easeOut.config(0.8), opacity: 0, left: -80 },
     { ease: Back.easeOut.config(0.8), opacity: 1, left: 0 },
-    0.05
+    // letterSpeed || 0.05
+    0
   );
 }
